8533024c1b9110fabaceea97031274a0fa237075
kooheng1993 <145702593+kooheng1993@users.noreply.github.com>
2024-05-23 00:26:06 +0800

    Create worker.js

---
+++ worker.js
@@ -0,0 +1,8 @@
+// worker.js
+self.onmessage = function(event) {
+    const { ownNumbers, outsideNumbers } = event.data;
+    const ownSet = new Set(ownNumbers);
+    const result = outsideNumbers.filter(number => !ownSet.has(number));
+
+    self.postMessage({ result });
+};
